946614a14e416481a161f6f893ac965f1077ca28
David Brown <davidb@davidb.org>
2025-09-27 15:57:22 -0600

    Attempt at a 3d visualization
    Render a 3d scene with the rocket and a planet.  This is almost useful, mostly being constrained by the complexity of rendering a large planet at different distances.
    
    Signed-off-by: David Brown <davidb@davidb.org>

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,12 @@ extern crate nalgebra as na;
 
 use std::io::Write;
 
-use bevy::prelude::*;
+use bevy::{
+    color::palettes::css::{GOLD, GREEN},
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+    pbr::{CascadeShadowConfigBuilder, Cascades, DirectionalLightShadowMap},
+    prelude::*,
+};
 
 /// The gravitational constant.
 const _G: f64 = 6.67430e-11;
@@ -16,42 +21,49 @@ const _G: f64 = 6.67430e-11;
 const _AU: f64 = 149_597_870_700.0;
 
 fn main() {
-    if false {
-        old_main();
-        return;
-    }
-
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins);
+    app.add_plugins((DefaultPlugins, FrameTimeDiagnosticsPlugin::default()));
     app.add_systems(Startup, setup);
     app.add_systems(Update, text_update_system);
+    app.add_systems(Update, text_update_fps);
+    app.add_systems(Update, keyboard_input_system);
 
     setup_sim(&mut app);
     app.run();
-
-    // App::new()
-    //     .add_plugins(DefaultPlugins.set(WindowPlugin {
-    //         window: WindowDescriptor {
-    //             title: "Orbital Mechanics Simulator".to_string(),
-    //             width: 800.0,
-    //             height: 600.0,
-    //             ..default()
-    //         },
-    //         ..default()
-    //     }))
-    //     .add_startup_system(setup)
-    //     .run();
 }
+
+#[derive(Resource)]
+struct Paused(bool);
+
+// Marker struct to identify the main camera.
+#[derive(Component)]
+struct MainCamera;
 
 // Marker struct to identify the text component.
 #[derive(Component)]
 struct StateText;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // UI camera
-    commands.spawn(Camera2d);
+// Marker for bodies, gives index into `body` in the sim.
+#[derive(Component)]
+struct BodyIndex(usize);
+
+// Marker for crafts, gives index into `craft` in the sim.
+#[derive(Component)]
+struct CraftIndex(usize);
+
+#[derive(Component)]
+struct FpsText;
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.insert_resource(Paused(false));
+
     commands.spawn((
-        Text::new("Hello World\nsecond line\nThis is a third line that is also longer."),
+        Text::new(""),
         TextFont {
             // This font is loaded and will be used instead of the default font.
             font: asset_server.load("fonts/FiraMono-Medium.ttf"),
@@ -69,17 +81,154 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         StateText,
     ));
 
-    // Camera
-    // commands.spawn((
-    //     Camera3d::default(),
-    //     Transform::from_xyz(0.0, 0.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
-    // ));
+    // Framerate display
+    commands
+        .spawn((
+            Text::new("FPS: "),
+            TextFont {
+                // This font is loaded and will be used instead of the default font.
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                font_size: 24.0,
+                ..default()
+            },
+            Node {
+                position_type: PositionType::Absolute,
+                top: Val::Px(5.0),
+                right: Val::Px(5.0),
+                ..default()
+            },
+        ))
+        .with_child((
+            TextSpan::default(),
+            TextFont {
+                // This font is loaded and will be used instead of the default font.
+                font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                font_size: 24.0,
+                ..default()
+            },
+            TextColor(GOLD.into()),
+            FpsText,
+        ));
+
+    // Create the main 3d camera.
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 2.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Projection::Perspective(PerspectiveProjection {
+            fov: std::f32::consts::FRAC_PI_3,
+            near: 0.1,
+            far: 1.0e12,
+            ..default()
+        }),
+        MainCamera,
+    ));
+
+    // Bring in the ship.
+    commands.spawn((
+        SceneRoot(
+            asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/output.gltf")),
+            // Make it slowly rotate.
+            // Rotate { axis: Vec3::Y, speed: 0.1 },
+        ),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+        CraftIndex(0),
+    ));
+
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 0.6,
+        ..Default::default()
+    });
+
+    commands.insert_resource(DirectionalLightShadowMap {
+        size: 8192,
+        ..default()
+    });
+
+    // The sun.
+    commands.spawn((
+        DirectionalLight {
+            illuminance: 30_000.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 10.0e8,
+            maximum_distance: 100.0e8,
+            ..default()
+        }
+        .build(),
+        Transform::from_rotation(Quat::from_euler(
+            EulerRot::XYZ,
+            std::f32::consts::PI,
+            0.0,
+            0.0,
+        )),
+    ));
+
+    let earth_mesh = Sphere { radius: 6.371e6 }.mesh().ico(40).unwrap();
+    let earth = meshes.add(earth_mesh);
+    let grass = materials.add(StandardMaterial {
+        base_color: GREEN.into(),
+        ..default()
+    });
+
+    // A sphere for the earth.
+    commands.spawn((
+        Mesh3d(earth),
+        MeshMaterial3d(grass),
+        Transform::from_xyz(0.0, -6.371e6 - 10.0, 0.0),
+        BodyIndex(0),
+    ));
+
+    // Create some objects "quads" in the distance.
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(5.0, 5.0))),
+        MeshMaterial3d(materials.add(Color::srgb(1.0, 0.0, 0.0))), // Red
+        Transform::from_xyz(0.0, 0.0, 0.0).with_rotation(
+            Quat::from_rotation_y(std::f32::consts::FRAC_PI_8)
+                * Quat::from_rotation_x(std::f32::consts::FRAC_PI_2),
+        ),
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(5.0, 5.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.0, 1.0, 0.0))), // Green
+        Transform::from_xyz(0.0, 0.0, -0.001).with_rotation(
+            Quat::from_rotation_y(std::f32::consts::FRAC_PI_8)
+                * Quat::from_rotation_x(std::f32::consts::FRAC_PI_2),
+        ),
+    ));
+}
+
+fn keyboard_input_system(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut paused: ResMut<Paused>,
+    mut time: ResMut<Time<Virtual>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::KeyP) {
+        paused.0 = !paused.0;
+
+        if paused.0 {
+            time.pause();
+        } else {
+            time.unpause();
+            // time.set_relative_speed(60.0);
+        }
+    }
 }
 
 fn text_update_system(
-    time: Res<Time>,
+    time: Res<Time<Virtual>>,
     mut sim: ResMut<Simulation>,
     mut query: Query<&mut Text, With<StateText>>,
+    paused: Res<Paused>,
+    mut bodies: Query<(&BodyIndex, &mut Transform), (With<BodyIndex>, Without<CraftIndex>)>,
+    mut crafts: Query<(&CraftIndex, &mut Transform), (With<CraftIndex>, Without<BodyIndex>)>,
+    mut cameras: Query<
+        &mut Transform,
+        (With<MainCamera>, (Without<CraftIndex>, Without<BodyIndex>)),
+    >,
 ) {
     // println!("text update {:.4}", time.elapsed_secs());
 
@@ -94,12 +243,109 @@ fn text_update_system(
 
     if let Ok(mut text) = query.single_mut() {
         let mut message = Vec::new();
+        if paused.0 {
+            writeln!(message, "Paused").unwrap();
+        } else {
+            writeln!(message, "Running").unwrap();
+        }
         writeln!(message, "Simulation time: {:.3} seconds", seconds).unwrap();
         sim.write(&mut message);
         writeln!(message, "{} physics steps", count).unwrap();
         **text = String::from_utf8(message).unwrap();
         // **text = format!("Some text now: {:.2} seconds", seconds);
         // println!("  subtext: {}", **text);
+    }
+
+    let mut camera = cameras.single_mut().unwrap();
+
+    // Update the crafts as needed.
+    for (index, mut position) in &mut crafts {
+        // For now, only update the first craft.
+        if index.0 != 0 {
+            continue;
+        }
+
+        // Craft rel planet.
+        let p_sim = sim.crafts[index.0].position - sim.bodies[0].position;
+        let v_sim = sim.crafts[index.0].velocity - sim.bodies[0].velocity;
+
+        // Tangent-frame basis in SIM space.
+        let up_sim = p_sim.normalize();
+
+        let vh_sim = {
+            let proj = v_sim.dot(&up_sim);
+            v_sim - up_sim * proj
+        };
+        let vhat_sim = if vh_sim.norm_squared() > 1e-12 {
+            vh_sim.normalize()
+        } else {
+            // If nearly vertical, just project a north.
+            let north = na::Vector3::new(0.0, 1.0, 0.0);
+            let nproj = north - up_sim * north.dot(&up_sim);
+            if nproj.norm_squared() > 0.0 {
+                nproj.normalize()
+            } else {
+                // If we are exactly at the pole, just pick something.
+                na::Vector3::x_axis().into_inner()
+            }
+        };
+
+        // right
+        let xhat_sim = up_sim.cross(&vhat_sim).normalize();
+        // forward (along track)
+        let zhat_sim = xhat_sim.cross(&up_sim).normalize();
+
+        // Camera offset in Sim space.
+        let back = 10.0;
+        // let back = 1.0e6;
+        let up_h = 2.0;
+        let cam_offset_sim = -back * zhat_sim + up_h * up_sim;
+
+        let up_b = sim_to_bevy(&up_sim);
+        // let zhat_b = sim_to_bevy(&zhat_sim);
+        let craft_pos_b = position.translation;
+        let cam_pos_b = craft_pos_b + sim_to_bevy(&cam_offset_sim);
+        // println!("Camera pos: {:?}", cam_pos_b);
+
+        camera.translation = cam_pos_b;
+        // camera.look_to(-zhat_b, up_b);
+        camera.look_at(craft_pos_b, up_b);
+
+        // Rotate the craft to match "up".  This is not actually correct, and it
+        // should be rotated according to the thrust vector, or it's own
+        // internal orientation, but we don't store that yet.
+        position.rotation = Quat::from_rotation_arc(Vec3::Y, up_b);
+    }
+
+    // Update the bodies.
+    for (index, mut position) in &mut bodies {
+        // The user's craft is the first craft.
+        let pos = sim.bodies[index.0].position - sim.crafts[0].position;
+        position.translation = sim_to_bevy(&pos);
+        // println!(
+        //     "Body {} position: {:?}, dist: {:04e}",
+        //     index.0,
+        //     pos,
+        //     position.translation.norm()
+        // );
+    }
+}
+
+/// Convert a sim vector to a Bevy vector.
+fn sim_to_bevy(v: &na::Vector3<f64>) -> Vec3 {
+    Vec3::new(v.x as f32, v.z as f32, -v.y as f32)
+}
+
+fn text_update_fps(
+    diagnostics: Res<DiagnosticsStore>,
+    mut query: Query<&mut TextSpan, With<FpsText>>,
+) {
+    if let Ok(mut text) = query.single_mut() {
+        if let Some(fps) = diagnostics.get(&FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(value) = fps.smoothed() {
+                **text = format!("{value:.2}");
+            }
+        }
     }
 }
 
@@ -108,8 +354,8 @@ fn setup_sim(app: &mut App) {
     let earth = Body::earth();
     let sun: Body = Body::sun();
 
-    // Create a ship that is just stuck 10m in the air above the surface.
-    let ship = Craft::new_above(&earth, 10.0);
+    // Create a ship that is just stuck 1km in the air above the surface.
+    let ship = Craft::new_above(&earth, 100.0);
 
     // Let's make a little force to test this.
     let thrust = Thrust {
@@ -122,52 +368,13 @@ fn setup_sim(app: &mut App) {
     let sim = Simulation {
         time: 0.0,
         collided: false,
-        step_time: 0.01,
-        print_time: 0.05,
+        step_time: 1.0 / 100.0,
         bodies: vec![earth, sun],
         crafts: vec![ship],
         thrust: Some(thrust),
     };
 
     app.insert_resource(sim);
-}
-
-fn old_main() {
-    // Make the basic earth.
-    let earth = Body::earth();
-    let sun: Body = Body::sun();
-
-    // Create a ship that is just stuck 10m in the air above the surface.
-    let ship = Craft::new_above(&earth, 10.0);
-    // let ship = Craft::new(
-    //     na::Vector3::new(0.0, 0.0, earth.radius + 10.0), // 10 m altitude
-    //     na::Vector3::new(0.0, 0.0, 0.0),                 // Stationary
-    //     200.0,
-    //     1.0,
-    // );
-
-    // Let's make a little force to test this.
-    let thrust = Thrust {
-        direction: (ship.position - earth.position).normalize(),
-        magnitude: 15.0, // Newtons
-        from: 0.5,       // seconds
-        until: 2.0,      // seconds
-    };
-
-    let mut sim = Simulation {
-        time: 0.0,
-        collided: false,
-        step_time: 0.01,
-        print_time: 0.05,
-        bodies: vec![earth, sun],
-        crafts: vec![ship],
-        thrust: Some(thrust),
-    };
-
-    sim.run();
-    sim.show();
-
-    println!("Final Sun position: {:?}", sim.bodies[1].position);
 }
 
 /// A "large" object in space.  This represents planets, and anything that has a
@@ -255,7 +462,7 @@ impl Craft {
     /// Create a new craft, above the given Body, initially, not moving.
     /// This basically assumes we are above the ecliptic north pole, which is very much not reasonable.
     fn new_above(body: &Body, altitude: f64) -> Self {
-        let loc = na::Vector3::new(1.0, 1.0, 0.2).normalize();
+        let loc = na::Vector3::new(-1.0, 2.0, 1.2).normalize();
 
         let position = body.position + loc * (body.radius + altitude);
         let rel_pos = position - body.position;
@@ -286,7 +493,6 @@ impl Craft {
 struct Simulation {
     time: f64,
     step_time: f64,
-    print_time: f64,
     collided: bool,
     bodies: Vec<Body>,
     crafts: Vec<Craft>,
@@ -321,6 +527,7 @@ impl Simulation {
         }
     }
 
+    #[allow(dead_code)]
     fn show(&self) {
         self.write(std::io::stdout());
     }
@@ -382,18 +589,6 @@ impl Simulation {
         }
 
         self.time += self.step_time;
-    }
-
-    /// Run the simulation, stopping when the craft intersects the surface of a body.
-    fn run(&mut self) {
-        let mut next_print = self.time + self.print_time;
-        while !self.collided {
-            self.step();
-            if self.time >= next_print {
-                self.show();
-                next_print += self.print_time;
-            }
-        }
     }
 }
 
