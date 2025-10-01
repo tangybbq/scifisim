90e29438d4825833cbd5cb75bc4cf4e363099cf1
David Brown <davidb@davidb.org>
2025-09-30 21:04:26 -0500

    Move spice wrapper into its own module
    Cleans up the solar module a bit.
    
    Signed-off-by: David Brown <davidb@davidb.org>

---
+++ src/solar/spice.rs
@@ -0,0 +1,68 @@
+/// Spice wrappers.
+use std::{
+    ffi::{CString, c_char},
+    sync::{LazyLock, Mutex},
+};
+
+pub static SPICE: LazyLock<Mutex<Spice>> = LazyLock::new(|| Mutex::new(Spice::new()));
+
+pub struct Spice;
+
+impl Spice {
+    pub fn new() -> Self {
+        // Load the SPICE kernels for use.
+        spice::furnsh("assets/spice/de440s.bsp");
+        spice::furnsh("assets/spice/naif0012.tls");
+        spice::furnsh("assets/spice/pck00011.tpc");
+        spice::furnsh("assets/spice/gm_de440.tpc");
+
+        Spice
+    }
+
+    pub fn str2et(&self, time: &str) -> f64 {
+        spice::str2et(time)
+    }
+
+    pub fn spkezr(
+        &self,
+        target: &str,
+        et: f64,
+        ref_frame: &str,
+        abcorr: &str,
+        observer: &str,
+    ) -> ([f64; 6], f64) {
+        spice::spkezr(target, et, ref_frame, abcorr, observer)
+    }
+
+    pub fn bodvrd(&self, body: &str, item: &str, maxn: usize) -> Vec<f64> {
+        spice::bodvrd(body, item, maxn)
+    }
+
+    pub fn pxform(&self, from: &str, to: &str, et: f64) -> [[f64; 3]; 3] {
+        spice::pxform(from, to, et)
+    }
+
+    pub fn sxform(&self, from: &str, to: &str, et: f64) -> [[f64; 6]; 6] {
+        let mut result = [[0.0; 6]; 6];
+        let from = CString::new(from).unwrap();
+        let to = CString::new(to).unwrap();
+        unsafe {
+            sxform_c(from.as_ptr(), to.as_ptr(), et, &mut result);
+        }
+        result
+    }
+
+    pub fn xf2rav(&self, xform: &[[f64; 6]; 6]) -> ([[f64; 3]; 3], [f64; 3]) {
+        let mut rot = [[0.0; 3]; 3];
+        let mut av = [0.0; 3];
+        unsafe {
+            xf2rav_c(xform, &mut rot, &mut av);
+        }
+        (rot, av)
+    }
+}
+
+unsafe extern "C" {
+    unsafe fn sxform_c(from: *const c_char, to: *const c_char, et: f64, xform: *mut [[f64; 6]; 6]);
+    unsafe fn xf2rav_c(xform: *const [[f64; 6]; 6], rot: *mut [[f64; 3]; 3], av: *mut [f64; 3]);
+}
