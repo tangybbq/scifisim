22e07dd4d2f19b33eac91a491df6720d5197bc6a
David Brown <davidb@davidb.org>
2025-10-01 17:06:34 -0500

    spice: Lock per call
    Instead of exposing the lock to the user, instead expose an object that
    can be cloned, and each method will call the lock internally.
    
    Signed-off-by: David Brown <davidb@davidb.org>

--- src/solar.rs
+++ src/solar.rs
@@ -6,8 +6,7 @@
 // for spice to actually be useful, we'll need to use our own lock, and just
 // make sure we only use the API while holding the lock.
 
-use std::sync::MutexGuard;
-
+use na::Matrix3x1;
 use nalgebra::{Matrix3, Vector3};
 
 mod spice;
@@ -17,11 +16,12 @@ pub struct Body {
     pub name: String,
     pub gm: f64, // Gravitational constant * mass, km^3/s^2
     pub radii: Vector3<f64>,
+    pub north: Matrix3x1<f64>,
 }
 
 impl Body {
-    pub fn new_from(id: i32) -> Option<Self> {
-        let sl = spice::SPICE.lock().unwrap();
+    pub fn new_from(id: i32, et: f64) -> Option<Self> {
+        let sl = spice::get_instance();
         let (name, has_name) = sl.bodc2n(id);
         if !has_name {
             return None;
@@ -41,33 +41,40 @@ impl Body {
             return None;
         }
         let radii = sl.bodvrd(&name, "RADII", 3);
+
+        let xform = sl.sxform(&format!("IAU_{}", name), "ECLIPJ2000", et);
+        let (rot, _av) = sl.xf2rav(&xform);
+        let rot = Matrix3::from_row_slice(&[
+            rot[0][0], rot[0][1], rot[0][2], // Row 0
+            rot[1][0], rot[1][1], rot[1][2], // Row 1
+            rot[2][0], rot[2][1], rot[2][2], // Row 2
+        ]);
+        let north = rot * Vector3::new(0.0, 0.0, 1.0);
+
         Some(Self {
             id,
             name,
             gm: gm[0],
             radii: Vector3::new(radii[0], radii[1], radii[2]),
+            north,
         })
     }
 }
 
 pub fn init_spice() {
-    if false {
-        let sl = spice::SPICE.lock().unwrap();
-        was_init_spice(&sl);
-    }
+    let sl = spice::get_instance();
+    // TODO: Better start date.
+    let et = sl.str2et("2024-01-01T00:00:00");
     let mut bodies = Vec::new();
     let mut start = 0;
     loop {
         let limit = 500;
-        let names = spice::SPICE
-            .lock()
-            .unwrap()
-            .gnpool("BODY*_GM", start, limit);
+        let names = sl.gnpool("BODY*_GM", start, limit);
         println!("Names: count: {}", names.len());
         for name in &names {
             let code = name[4..name.len() - 3].parse::<i32>().unwrap();
 
-            if let Some(body) = Body::new_from(code) {
+            if let Some(body) = Body::new_from(code, et) {
                 if body.gm > 1.0 && !body.name.ends_with(" BARYCENTER") {
                     bodies.push(body);
                 }
@@ -92,6 +99,7 @@ pub fn init_spice() {
     println!("Interesting: {}", bodies.len());
 }
 
+/*
 pub fn was_init_spice(sl: &MutexGuard<'_, spice::Spice>) {
     let et = sl.str2et("2024-01-01T00:00:00");
     let (state, _) = sl.spkezr("EARTH", et, "ECLIPJ2000", "NONE", "SSB");
@@ -130,3 +138,4 @@ pub fn was_init_spice(sl: &MutexGuard<'_, spice::Spice>) {
     let (position, light_time) = sl.spkezr("SUN", et, "ECLIPJ2000", "NONE", "SSB");
     println!("position: {position:?}, light_time: {light_time:?}");
 }
+*/
--- src/solar/spice.rs
+++ src/solar/spice.rs
@@ -1,12 +1,19 @@
 /// Spice wrappers.
 use std::{
     ffi::{CStr, CString},
-    sync::{LazyLock, Mutex},
+    sync::{Arc, LazyLock, Mutex},
 };
 
-pub static SPICE: LazyLock<Mutex<Spice>> = LazyLock::new(|| Mutex::new(Spice::new()));
+/// The single global SPICE instance.
+static SPICE: LazyLock<Spice> = LazyLock::new(|| Spice::new());
 
-pub struct Spice;
+pub fn get_instance() -> Spice {
+    SPICE.clone()
+}
+
+/// A wrapped SPICE interface.  Internally cares for its own locking.
+#[derive(Clone)]
+pub struct Spice(Arc<Mutex<()>>);
 
 impl Spice {
     pub fn new() -> Self {
@@ -16,13 +23,26 @@ impl Spice {
         spice::furnsh("assets/spice/pck00011.tpc");
         spice::furnsh("assets/spice/gm_de440.tpc");
 
-        Spice
+        Spice(Arc::new(Mutex::new(())))
     }
 
+    // TODO: Instead of this access, just set to RETURN, and do the check/reset
+    // on each call, and use a Result for the returns.
+    /*
+    pub fn erract(&self) {
+        let _lock = self.0.lock().unwrap();
+        unsafe {
+            spice::c::erract_c();
+        }
+    }
+    */
+
     pub fn str2et(&self, time: &str) -> f64 {
+        let _lock = self.0.lock().unwrap();
         spice::str2et(time)
     }
 
+    #[allow(dead_code)]
     pub fn spkezr(
         &self,
         target: &str,
@@ -31,22 +51,28 @@ impl Spice {
         abcorr: &str,
         observer: &str,
     ) -> ([f64; 6], f64) {
+        let _lock = self.0.lock().unwrap();
         spice::spkezr(target, et, ref_frame, abcorr, observer)
     }
 
     pub fn bodvrd(&self, body: &str, item: &str, maxn: usize) -> Vec<f64> {
+        let _lock = self.0.lock().unwrap();
         spice::bodvrd(body, item, maxn)
     }
 
     pub fn bodfnd(&self, body: i32, item: &str) -> bool {
+        let _lock = self.0.lock().unwrap();
         spice::bodfnd(body, item)
     }
 
+    #[allow(dead_code)]
     pub fn pxform(&self, from: &str, to: &str, et: f64) -> [[f64; 3]; 3] {
+        let _lock = self.0.lock().unwrap();
         spice::pxform(from, to, et)
     }
 
     pub fn sxform(&self, from: &str, to: &str, et: f64) -> [[f64; 6]; 6] {
+        let _lock = self.0.lock().unwrap();
         let mut result = [[0.0; 6]; 6];
         let from = CString::new(from).unwrap();
         let to = CString::new(to).unwrap();
@@ -62,6 +88,7 @@ impl Spice {
     }
 
     pub fn xf2rav(&self, xform: &[[f64; 6]; 6]) -> ([[f64; 3]; 3], [f64; 3]) {
+        let _lock = self.0.lock().unwrap();
         let mut rot = [[0.0; 3]; 3];
         let mut av = [0.0; 3];
         unsafe {
@@ -71,6 +98,7 @@ impl Spice {
     }
 
     pub fn gnpool(&self, name: &str, start: usize, room: usize) -> Vec<String> {
+        let _lock = self.0.lock().unwrap();
         let mut buf = vec![[0u8; 33]; room];
         let mut n = 0;
         let mut found = 0;
@@ -98,10 +126,12 @@ impl Spice {
 
     #[allow(dead_code)]
     pub fn gdpool(&self, name: &str, start: usize, room: usize) -> Vec<f64> {
+        let _lock = self.0.lock().unwrap();
         spice::gdpool(name, start, room)
     }
 
     pub fn bodc2n(&self, code: i32) -> (String, bool) {
+        let _lock = self.0.lock().unwrap();
         spice::bodc2n(code)
     }
 }
